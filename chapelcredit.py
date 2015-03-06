419ca1fee05dc5ae3b6876e7b32238b60712cbff
Adam Vigneaux <adamvigneaux@gmail.com>
2015-03-05 23:28:42 -0500

    Add loginMyGordon and getMealPoints functions

--- chapelcredit.py
+++ chapelcredit.py
@@ -5,6 +5,45 @@ from flask.ext.cors import CORS, cross_origin
 app = Flask(__name__)
 CORS(app)
 
+# Login to My.Gordon.edu with given credentials
+# Returns browser instance
+def loginMyGordon(username, password, browser):
+    browser.open("https://my.gordon.edu/ics")
+
+    browser.select_form(name="MAINFORM")
+    browser['userName'] = username
+    browser['password'] = password
+    browser.submit()
+
+    return browser
+
+# Get meal points from My.Gordon.edu for given user
+# Returns number of meal points
+def getMealPoints(username, password, browser):
+    mealPoints = {}
+
+    # Login and navigate to mealpoints page
+    browser = loginMyGordon(username, password, browser)
+    mealpointsLink = browser.open('/ICS/Students/Mealpoints.jnz')
+
+    # Parse HTML to find URL that iFrame points to
+    soup = BeautifulSoup(browser.response().read())
+    iframeSrc = soup.find('iframe')['src']
+
+    # Navigate to page that displays mealpoints
+    browser.open('https://my.gordon.edu' + iframeSrc)
+    browser.open('https://my.gordon.edu/GMEX')
+
+    if browser.response().code == 200:
+        # Parse HTML to find mealpoints
+        soup = BeautifulSoup(browser.response().read())
+        mainTable = soup.find_all('table')[1]
+        pointsRow = mainTable.find_all('tr')[0]
+        pointsCell = pointsRow.find_all('td')[1].find('span')
+        mealPoints = { 'mealpoints': pointsCell.string }
+
+    return app.make_response((json.dumps(mealPoints), browser.response().code))
+
 # Get chapel credit from Go.Gordon.edu with given credentials
 # Returns dictionary with:
 #   credit   (int)   User's credits
