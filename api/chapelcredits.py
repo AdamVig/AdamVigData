f6a1f9e38adbfac37e5c495c9ec9afb25ab8b179
Adam Vigneaux <adamvigneaux@gmail.com>
2015-04-08 23:10:16 -0400

    Add support for "No CL&W Credit Found" (resolves #5)

--- api/chapelcredits.py
+++ api/chapelcredits.py
@@ -5,27 +5,32 @@ def get_chapel_credits(username, password):
     """Get chapel credits from Go Gordon"""
 
     url = 'go.gordon.edu/student/chapelcredits/viewattendance.cfm'
-    response = requests.get('https://' + username + ':' + password + '@' + url)
+    # response = requests.get('https://' + username + ':' + password + '@' + url)
+    response = requests.get('http://127.0.0.1:8100/test.html')
 
     if response.status_code == 200:
-        page = BeautifulSoup(response.text)
 
-        credit_table = page.find_all('table')[8]
+        if not "No Christian Life and Worship Credit Found" in response.text:
+            page = BeautifulSoup(response.text)
 
-        credits = credit_table \
-            .find_all('tr')[0] \
-            .find_all('td')[1] \
-            .text
+            credit_table = page.find_all('table')[8]
 
-        required = credit_table \
-            .find_all('tr')[1]  \
-            .find_all('td')[1]  \
-            .text
+            credits = credit_table \
+                .find_all('tr')[0] \
+                .find_all('td')[1] \
+                .text
 
-        return {
-            'data': int(credits),
-            'outof': int(required)
-        }, 200
+            required = credit_table \
+                .find_all('tr')[1]  \
+                .find_all('td')[1]  \
+                .text
+
+            return {
+                'data': int(credits),
+                'outof': int(required)
+            }, 200
+        else:
+            return "No chapel credits found.", 404
 
     else:
         return "Chapel credits are unavailable", response.status_code
