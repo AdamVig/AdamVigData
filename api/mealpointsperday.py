3243646c9f45dfe08a3969c1907910930d82e9d9
Adam Vigneaux <adamvigneaux@gmail.com>
2015-04-17 02:35:26 -0400

    Add invalid login handling to /mealpointsperday

--- api/mealpointsperday.py
+++ api/mealpointsperday.py
@@ -3,7 +3,14 @@ import mealpoints, daysleftinsemester
 def get_meal_points_per_day(username, password):
     """Get meal points per day"""
 
-    meal_points = mealpoints.get_meal_points(username, password)[0]['data']
+    meal_points = mealpoints.get_meal_points(username, password)
+
+    # Handle meal points error
+    if meal_points[1] == 200:
+        meal_points = meal_points[0]['data']
+    else:
+        return meal_points
+
     days = daysleftinsemester.get_days_left_in_semester()[0]['data']
 
     if meal_points > 0 and days > 0:
