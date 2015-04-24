412b53de584d5af0e8fb25ec017162084d834a95
Adam Vigneaux <adamvigneaux@gmail.com>
2015-04-24 17:59:32 -0400

    Add error handling for improper Base64 encoding (fixes #15)

--- api/services/getcredentials.py
+++ api/services/getcredentials.py
@@ -8,4 +8,9 @@ def get_credentials(request):
     username = request.args.get('username')
     password = request.args.get('password')
     password = base64.b64decode(password)
+    try:
+        password = base64.b64decode(password)
+    except:
+        raise ValueError("Password is not properly Base64 encoded.")
+
     return username, password
