b04dcf73f6272bb4ab2cb482b59ab6f26102ed79
Adam Vigneaux <adamvigneaux@gmail.com>
2015-04-25 14:50:46 -0400

    Add error handling to Mechanize browser in chapelcredit.py

--- api/chapelcredits.py
+++ api/chapelcredits.py
@@ -1,4 +1,4 @@
-import mechanize
+import mechanize, urllib2
 from bs4 import BeautifulSoup
 
 def get_chapel_credits(username, password):
@@ -9,32 +9,41 @@ def get_chapel_credits(username, password):
     browser = mechanize.Browser()
     browser.set_handle_robots(False)
     browser.add_password(url, username, password)
-    browser.open(url)
 
-    if browser.response().code == 200:
+    try:
+        browser.open(url)
 
-        if not "No Christian Life and Worship Credit Found" in browser.response().read():
-            page = BeautifulSoup(browser.response().read())
-
-            credit_table = page.find_all('table')[8]
-
-            credits = credit_table \
-                .find_all('tr')[0] \
-                .find_all('td')[1] \
-                .text
-
-            required = credit_table \
-                .find_all('tr')[1]  \
-                .find_all('td')[1]  \
-                .text
-
-            return {
-                'data': int(credits),
-                'outof': int(required)
-            }
+    except urllib2.HTTPError as err:
+        if err.code == 401:
+            raise ValueError("Username and password do not match.", 401)
         else:
-            raise ValueError("No chapel credits found.", 404)
-    elif browser.response().code == 401:
-        raise ValueError("Username and password do not match.", 401)
+            raise ValueError("HTTPError: Chapel credits are unavailable.", 500)
+
+    except Exception as err:
+        raise ValueError("Chapel credits are unavailable.", 500)
+
     else:
-        raise ValueError("Chapel credits are unavailable.", browser.response().code)
+        response = browser.response().read()
+
+    if "No Christian Life and Worship Credit Found" in response:
+        raise ValueError("No chapel credits found.", 404)
+
+    # Find chapel credits on page
+    page = BeautifulSoup(response)
+
+    credit_table = page.find_all('table')[8]
+
+    credits = credit_table \
+        .find_all('tr')[0] \
+        .find_all('td')[1] \
+        .text
+
+    required = credit_table \
+        .find_all('tr')[1]  \
+        .find_all('td')[1]  \
+        .text
+
+    return {
+        'data': int(credits),
+        'outof': int(required)
+    }
