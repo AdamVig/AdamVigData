4e341c36c665438897a9a62c20544b613edf9f4e
Adam Vigneaux <adamvigneaux@gmail.com>
2015-05-04 23:50:51 -0400

    Fix undefined variables and multi-line statement debacle

--- api/services/log.py
+++ api/services/log.py
@@ -5,22 +5,25 @@ def create_log(request, response):
 
     separator = ' '
     log_message = ''
+    method = request.method
+    status = str(response.status_code)
+    path = request.path
 
     if method != 'GET':
-        log_message += 'method={method}{separator}'
-            .format(method=request.method, separator=separator)
+        log_message += 'method={method}{separator}' \
+            .format(method=method, separator=separator)
 
     if status != str(httplib.OK):
-        log_message += 'status={status}{separator}'
-            .format(status=str(response.status_code), separator=separator)
+        log_message += 'status={status}{separator}' \
+            .format(status=status, separator=separator)
 
-    log_message += 'path={path}{separator}'
-        .format(path=request.path, separator=separator)
+    log_message += 'path={path}{separator}' \
+        .format(path=path, separator=separator)
 
     if 'username' in request.args and 'password' in request.args:
-        log_message += 'user={username}{separator}pass={password}'
-            .format(username=request.args.get('username'),
-                    password=request.args.get('password'),
+        log_message += 'user={username}{separator}pass={password}' \
+            .format(username=request.args.get('username'), \
+                    password=request.args.get('password'), \
                     separator=separator)
 
     return log_message
