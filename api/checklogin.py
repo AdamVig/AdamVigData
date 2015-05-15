ec7340603ecc2e043ea1ad4da1d313d5e05a99b1
Adam Vigneaux <adamvigneaux@gmail.com>
2015-05-14 21:16:37 -0400

    Change /checklogin to rely on HEAD method for faster performance

--- api/checklogin.py
+++ api/checklogin.py
@@ -1,33 +1,29 @@
-import mechanize, urllib2, httplib, services
+import mechanize, urllib2, httplib, requests, services
 
 def check_login(username, password):
     """Check login on Go Gordon"""
 
-    # Retrieve page
     url = 'https://go.gordon.edu/'
-    browser = mechanize.Browser()
-    browser.set_handle_robots(False)
-    browser.add_password(url, username, password)
+    auth = username + ':' + password + '@'
 
     try:
-        browser.open(url)
+        response = requests.head(url, auth=(username, password))
 
-    except urllib2.HTTPError as err:
-        if err.code == httplib.UNAUTHORIZED:
-            raise ValueError("Invalid login.",
-                httplib.UNAUTHORIZED)
-        else:
-            raise ValueError("HTTPError: Unexpected error.",
-                httplib.INTERNAL_SERVER_ERROR)
-
-    except Exception as err:
-        raise ValueError("Unexpected error.",
-            httplib.INTERNAL_SERVER_ERROR)
+    except requests.exceptions.RequestException as e:
+        raise ValueError("Unexpected error.", httplib.INTERNAL_SERVER_ERROR)
 
     else:
-        try:
-            user = services.db.get_user(username)
-        except ValueError as err:
-            return "Valid login but user does not exist in database."
+        if response.status_code == httplib.OK:
+            try:
+                user = services.db.get_user(username)
+            except ValueError as err:
+                print "Valid login but user does not exist in database."
+            else:
+                return { 'data': user }
+
+
+        elif response.status_code == httplib.UNAUTHORIZED:
+            raise ValueError("Invalid login.", httplib.UNAUTHORIZED)
+
         else:
-            return { 'data': user }
+            raise ValueError("Unexpected error.", httplib.INTERNAL_SERVER_ERROR)
