8a812304a3029e21ce087bc82dcf7fee4f4ee5e3
Adam Vigneaux <adamvigneaux@gmail.com>
2015-09-21 19:16:20 -0400

    Mark users who have not logged in as inactive

--- mark_inactive.py
+++ mark_inactive.py
@@ -1,11 +1,16 @@
 """Mark all users inactive in the database."""
 from api.services import *
 
+reserved_ids = ['cache', 'info', 'message']
 database = db.get_db()
 
 for doc_id in database:
-    if doc_id != 'info':
+    if doc_id not in reserved_ids:
         doc = db.get_doc(doc_id, database)
-        doc['active'] = False
-        database.save(doc)
-        print "Marked " + doc_id + " as inactive."
+        if 'totalLogins' in doc:
+            if doc['totalLogins'] == 0:
+                doc['active'] = False
+                database.save(doc)
+                print "Marked " + doc_id + " as inactive."
+        else:
+            print "User " + doc_id + " does not have a totalLogins field."
